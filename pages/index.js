bc7e4ab92b7b1a1c6157f53a1cd0990c38379aca
muscleheadDeveloper <njokua04@gmail.com>
2022-11-28 11:43:26 +0100

    starter code

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React, {useContext} from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+
+import axios from 'axios'
+export default function Auth() {
+  return <div className="background">auth</div>;
+}
